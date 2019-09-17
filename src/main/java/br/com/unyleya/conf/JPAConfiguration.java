6516d638317f7b94fa0ef2df7d42304b4d4ba571
leonardofaria00 <leonardofaria00@gmail.com>
2019-09-17 11:56:45 -0300

    [FIX] Implementado classe de configuração do ORM

---
+++ src/main/java/br/com/unyleya/conf/JPAConfiguration.java
@@ -0,0 +1,44 @@
+package br.com.unyleya.conf;
+
+import java.util.Properties;
+
+import javax.persistence.EntityManagerFactory;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@EnableTransactionManagement
+public class JPAConfiguration {
+
+	@Bean
+	public LocalContainerEntityManagerFactoryBean localContainerEntityManagerFactoryBean() {
+		LocalContainerEntityManagerFactoryBean bean = new LocalContainerEntityManagerFactoryBean();
+		JpaVendorAdapter vendor = new HibernateJpaVendorAdapter();
+		bean.setJpaVendorAdapter(vendor);
+
+		DriverManagerDataSource source = new DriverManagerDataSource();
+		source.setUsername("postgres");
+		source.setPassword("lnx4frdm");
+		source.setUrl("jdbc:postgresql://localhost:5432/banco_unyleya");
+		source.setDriverClassName("org.postgresql.Driver");
+		bean.setDataSource(source);
+		Properties props = new Properties();
+		props.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+		props.setProperty("hibernate.show_sql", "true");
+		props.setProperty("hibernate.hbm2ddl.auto", "update");
+		bean.setJpaProperties(props);
+
+		bean.setPackagesToScan("br.com.unyleya.model");
+		return bean;
+	}
+
+	@Bean
+	public JpaTransactionManager transactionManager(EntityManagerFactory emf) {
+		return new JpaTransactionManager(emf);
+	}
+}
